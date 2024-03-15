2247b58d48824ae33bbba7d4e156e74150d79833
PabCeb <pablo.ceballos.08@gmail.com>
2024-03-15 01:32:31 -0300

    creacion de formularios html

--- inicio/views.py
+++ inicio/views.py
@@ -13,6 +13,18 @@ def pacientes(request):
     return render(request, 'pacientes.html', {'pacientes': pacientes})
 
 def agregar_paciente(request):
-    paciente = Paciente(nombre = 'nombre1', apellido = 'ape1', dni = 12345678, plan = 'plan310')
-    paciente.save()
-    return render(request, 'agregar_paciente.html', {'paciente': paciente})
+    # paciente = Paciente(nombre = 'nombre1', apellido = 'ape1', dni = 12345678, plan = 'plan310')
+    # paciente.save()
+    # return render(request, 'agregar_paciente.html', {'paciente': paciente})
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        apellido = request.POST.get('apellido')
+        dni = request.POST.get('dni')
+        plan = request.POST.get('plan')
+        paciente = Paciente(nombre = nombre, apellido = apellido, dni = dni, plan = plan)
+        paciente.save()
+    
+    
+    
+    
+    return render(request, 'agregar_paciente.html', {})
