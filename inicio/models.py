246264f5613d6f0bc38f8d04123b1ad65d0cbc69
PabCeb <pablo.ceballos.08@gmail.com>
2024-03-15 00:07:04 -0300

    carga de models

--- inicio/models.py
+++ inicio/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Paciente(models.Model):
+    nombre = models.CharField(max_length = 20)
+    apellido = models.CharField(max_length = 20)
+    # id = models.IntegerField()
+    plan = models.CharField(max_length = 10)
+    
