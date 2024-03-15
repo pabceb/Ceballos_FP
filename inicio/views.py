4a0d974f97c0918f3a7db54df2c124a7261c2368
PabCeb <pablo.ceballos.08@gmail.com>
2024-03-15 00:19:55 -0300

    creacion de templates

--- inicio/views.py
+++ inicio/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def inicio(request):
-    return HttpResponse('Hola Mundo!')
+    # return HttpResponse('Hola Mundo!')
+    diccionario = {}
+    return render(request, 'index.html', diccionario)
 
 def pacientes(request):
     ...
